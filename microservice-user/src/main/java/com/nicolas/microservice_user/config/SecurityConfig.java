2ce1392e2b4884d645ae1c2d20d69cd690476368
NicolasPerezU <u20221204357@usco.edu.co>
2025-03-03 15:19:16 -0500

    keycloak

--- microservice-user/src/main/java/com/nicolas/microservice_user/config/SecurityConfig.java
+++ microservice-user/src/main/java/com/nicolas/microservice_user/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.nicolas.microservice_user.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,20 +16,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorize->
-                        authorize
-                                .requestMatchers("/users/**").permitAll()
-                                .requestMatchers("/reservations/**").permitAll()
-                                .anyRequest().permitAll())
-
-                .sessionManagement(session->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        return http.build();
+        return http
+                .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated()) // Reemplazo de authorizeExchange()
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())) // Nueva forma de configurar JWT
+                .build();
     }
-
 
 }
